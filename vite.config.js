68f826230dc12354e8e585cc1c30f00721b543ad
Herweez <tomm.kwikys@gmail.com>
2026-04-15 02:37:22 +0200

    Set base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Website/',
   server: {
     port: 3000,
-    open: true
+    open: false
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
   }
 })
