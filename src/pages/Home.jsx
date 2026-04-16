44194cd8e4f820f9f798f6c2f8aafa5c02a16241
Herweez <tomm.kwikys@gmail.com>
2026-04-16 07:05:21 +0200

    Přidej Beam Me Up (Kill Mode) od Cazzette do featured tracks

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,9 @@ import './Home.css'
 
 export default function Home() {
   const featuredTracks = [
+    { title: 'BEAM ME UP (KILL MODE)', artist: 'Cazzette', url: '/audio/cazzette%20%20%20beam%20me%20up%20(kill%20mode)%20(radio%20edit)%20-%20[MP3JUICES.COM].mp3' },
     { title: 'DIGITAL DREAMS', artist: 'Cyberpower', url: '/audio/digital-dreams.mp3' },
     { title: 'NEON NIGHTS', artist: 'Cyberpower', url: '/audio/neon-nights.mp3' },
-    { title: 'CYBER PULSE', artist: 'Cyberpower', url: '/audio/cyber-pulse.mp3' },
   ]
 
   return (
